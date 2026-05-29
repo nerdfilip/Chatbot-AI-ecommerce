6bbbbf8a7ff906b92232bde80647a180be176592
Filip Madalin-Andrei <filip.madalin2@gmail.com>
2026-05-29 14:02:02 +0300

    ngrok deployment logic

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = 'https://daren-plumular-monet.ngrok-free.dev/api';
+const API_BASE = 'https://b3de-81-196-7-134.ngrok-free.app/api';
 
 export const getProducts = async (params = {}) => {
   const response = await axios.get(`${API_BASE}/products`, { params });

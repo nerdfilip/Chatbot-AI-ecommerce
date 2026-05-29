8b04d4afaa661880276509a9b5ee44da381b0b96
Filip Madalin-Andrei <filip.madalin2@gmail.com>
2026-05-29 14:19:39 +0300

    fix ngrok header

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,17 +2,24 @@ import axios from 'axios';
 
 const API_BASE = 'https://exception-sectional-skittle.ngrok-free.dev/api';
 
+const axiosInstance = axios.create({
+  baseURL: API_BASE,
+  headers: {
+    'ngrok-skip-browser-warning': 'true'
+  }
+});
+
 export const getProducts = async (params = {}) => {
-  const response = await axios.get(`${API_BASE}/products`, { params });
+  const response = await axiosInstance.get('/products', { params });
   return response.data;
 };
 
 export const getCategories = async () => {
-  const response = await axios.get(`${API_BASE}/categories`);
+  const response = await axiosInstance.get('/categories');
   return response.data;
 };
 
 export const getProduct = async (id) => {
-  const response = await axios.get(`${API_BASE}/products/${id}`);
+  const response = await axiosInstance.get(`/products/${id}`);
   return response.data;
 };
